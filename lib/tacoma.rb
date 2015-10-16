6cd9a6628aee7959f3d04bf180065516b8d8a229
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-10-16 09:42:37 +0200

    Tacoma::Tool.files instead of Tacoma::TOOLS

--- lib/tacoma.rb
+++ lib/tacoma.rb
@@ -4,18 +4,26 @@ require_relative './tacoma/command'
 require_relative './tacoma/version'
 
 module Tacoma
-  TOOLS = {
-    fog: '.fog',
-    boto: '.boto',
-    s3cfg: '.s3cfg',
-    route53: '.route53',
-    aws_credentials: '.aws/credentials'
-  }
+  class Tool
+    def self.files
+      TOOLS # pre-removal commit
+    end
+
+    private
+
+    TOOLS = {
+      fog: '.fog',
+      boto: '.boto',
+      s3cfg: '.s3cfg',
+      route53: '.route53',
+      aws_credentials: '.aws/credentials'
+    }
+  end
 
   module_function
   # NOTE: removes the first char (dot) for tool files
   def template_file(tool)
-    tool ? TOOLS[tool][1..-1] : 'tacoma.yml'
+    tool ? Tool.files[tool][1..-1] : 'tacoma.yml'
   end
 
   def gem_template_file(tool = nil)
